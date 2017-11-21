c94ce9dc3c4c07abd707491d8bc2510dd5d50749
Weiran Xiong <weiranxiong@Weirans-MacBook-Pro.local>
2017-11-20 17:53:36 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Nav from "./component/Nav.js";
+import Main from './component/Main.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App row">
+          <Nav />
+          <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Nav.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <div className="Nav col-lg-3">
+                <nav className="">
+                    <div classname="">
+                        <h1 className="main-header">Weiran</h1>
+                        <ul className="nav-list">
+                            <li>home</li>
+                            <li>about</li>
+                            <li>contact</li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+
+}
+
+export default Nav;
