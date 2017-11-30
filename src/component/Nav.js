0f15065f315e0a9c1f99dd67e8bd67334bb4c79d
Weiran Xiong <weiranxiong@Weirans-MacBook-Pro.local>
2017-11-29 20:11:52 -0800

    Domain

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -7,12 +7,16 @@ class Nav extends Component {
             <div className="Nav col-lg-3">
                 <nav className="">
                     <div classname="">
-                        <h1 className="main-header">Weiran</h1>
+                        <h1 className="main-header">weiran.code()</h1>
                         <ul className="nav-list">
                             <li>home</li>
                             <li>about</li>
                             <li>contact</li>
                         </ul>
+                        <div className="building-notice container">
+                            <p>Still building this site...</p>
+                            <p>Open Source on <a href="https://github.com/weiranx/personal-website">Github</a></p>
+                        </div>
                     </div>
                 </nav>
             </div>
